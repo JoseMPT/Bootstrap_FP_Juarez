aeb5c7dd239b641d88195a8250befc97b777d23c
JoseMPT <s20030132@itsch.edu.mx>
2023-05-18 09:12:22 -0600

    Se añadió información en index.php, contact.php y footer_content.hphp.

--- contact.php
+++ contact.php
@@ -32,7 +32,7 @@
 
                 <div>
                     <iframe allowfullscreen
-                            -frameborder="0"
+                            frameborder="0"
                             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" style="border:0; width: 100%; height: 270px;"></iframe>
                 </div>
 
@@ -91,7 +91,7 @@
                             <div class="my-3">
                                 <div class="loading">Loading</div>
                                 <div class="error-message"></div>
-                                <div class="sent-message">Your message has been sent. Thank you!</div>
+                                <div class="sent-message">Tus comentarios han sido enviados. ¡Gracias!</div>
                             </div>
                             <div class="text-center">
                                 <button type="submit">Enviar comentarios<i class="fi fi-sr-paper-plane"></i></button>

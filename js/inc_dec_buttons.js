99d68055020094fd5171e9b289f754064058bf4c
JoseMPT <s20030132@itsch.edu.mx>
2023-05-21 17:58:39 -0600

    Mejora del despliegue de products.php a través de ajax con jquery.

---
+++ js/inc_dec_buttons.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    let buttonLess = $('.button-less')
+    let buttonMore = $('.button-more')
+    let inputAmount = $('.select-amount')
+    let inc = (event) =>{
+        let button = event.target
+        let input = $(button).prev()
+        if (input.val() < 99) input.val(parseInt(input.val())+1)
+    }
+    let dec = (event) =>{
+        let button = event.target
+        let input = $(button).next()
+        if(input.val() > 1) input.val(parseInt(input.val())-1)
+    }
+
+    let amount = (event) =>{
+        let input = $(event.target)
+        if (input.val() === '') input.val(1)
+    }
+
+    buttonLess.on('click', dec)
+    buttonMore.on('click', inc)
+    inputAmount.on('change', amount)
+});
--- products.php
+++ products.php
@@ -1,6 +1,21 @@
+<?php
+include_once 'php_PDO/PDO_Connection.php';
+
+$connection = new PDO_Connection();
+$pdo = $connection->getPDO();
+
+$command = $pdo->prepare('SELECT product_id,product_name,product_description,product_unitCost FROM products_data;');
+$command->execute();
+
+$result = $command->fetchAll(PDO::FETCH_CLASS)
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="es">
 <?php include_once './php_include/head_content.hphp'?>
+
 <body>
     <?php include_once './php_include/header_content.hphp'?>
     <main>
@@ -23,9 +38,9 @@
         <!-- ======= Blog Section ======= -->
         <section class="blog" id="blog">
             <div class="container" data-aos="fade-up">
-
                 <div class="row">
-                    <div class="col-lg-4">
+                    <!-- Barra lateral -->
+                    <div class="col-lg-12">
                         <div class="sidebar">
                             <h3 class="sidebar-title">Búsqueda</h3>
                             <div class="sidebar-item search-form">
@@ -40,17 +55,14 @@
                             <h3 class="sidebar-title">Categories</h3>
                             <div class="sidebar-item categories">
                                 <ul>
-                                    <li><a href="#">General <span>(25)</span></a></li>
-                                    <li><a href="#">Lifestyle <span>(12)</span></a></li>
-                                    <li><a href="#">Travel <span>(5)</span></a></li>
-                                    <li><a href="#">Design <span>(22)</span></a></li>
-                                    <li><a href="#">Creative <span>(8)</span></a></li>
-                                    <li><a href="#">Educaion <span>(14)</span></a></li>
+                                    <li><a href="#">Herramientas <span></span></a></li>
+                                    <li><a href="#">Materiales <span></span></a></li>
                                 </ul>
                             </div>
                             <!-- Fin de categorías -->
 
-                            <h3 class="sidebar-title">Recent Posts</h3>
+                            <!-- Recientes -->
+                            <!--<h3 class="sidebar-title">Recent Posts</h3>
                             <div class="sidebar-item recent-posts">
                                 <div class="post-item clearfix">
                                     <img alt="" src="assets/img/blog/blog-recent-1.jpg">
@@ -83,15 +95,15 @@
                                     <time datetime="2020-01-01">Jan 1, 2020</time>
                                 </div>
 
-                            </div>
+                            </div>-->
                             <!-- Fin de productos recientes -->
 
                             <h3 class="sidebar-title">Etiquetas</h3>
                             <div class="sidebar-item tags">
                                 <ul>
-                                    <li><a href="#">App</a></li>
-                                    <li><a href="#">IT</a></li>
-                                    <li><a href="#">Business</a></li>
+                                    <li><a href="#">Ferretería</a></li>
+                                    <li><a href="#">Plomería</a></li>
+                                    <!--<li><a href="#">Business</a></li>
                                     <li><a href="#">Mac</a></li>
                                     <li><a href="#">Design</a></li>
                                     <li><a href="#">Office</a></li>
@@ -99,7 +111,7 @@
                                     <li><a href="#">Studio</a></li>
                                     <li><a href="#">Smart</a></li>
                                     <li><a href="#">Tips</a></li>
-                                    <li><a href="#">Marketing</a></li>
+                                    <li><a href="#">Marketing</a></li>-->
                                 </ul>
                             </div>
                             <!-- Final de etiquetas -->
@@ -109,26 +121,33 @@
 
                     </div>
                     <!-- Fin de barra lateral de búsqueda -->
-
-                    <div class="col-lg-8 entries">
-                        <form class="form-product php-email-form" name="form-data-materials" method="get" action="user_add_shoppingCart.php">
-                            <input class="product-id" type="hidden" name="product_Id" value="1">
+                </div>
+                <div class="row">
+                    <!-- Despliegue de productos -->
+                    <?php foreach ($result as $item):?>
+                    <div class="col-lg-12 entries">
+                        <form class="form-product php-email-form" name="form-data-materials" method="get" action="php_forms/add-product-to-cart.hphp">
+                            <input class="product-id" type="hidden" name="product-id" value="<?=$item->product_id?>">
                             <i class="fi fi-sr-box-open icon-product"></i>
                             <!--<img class="icon_products" src="icons/product.svg" alt="product">-->
                             <div class="div-product-data">
-                                <h3 class="product-name">Martillo</h3>
-                                <p class="product-description">Un martillo de hierro</p>
-                                <p class="product-description">Precio: $600</p>
-                                <p class="product-description">
-                                    <label for="select-amount">Cantidad a comprar: </label>
-                                    <input id="select-amount" type="number" min="1" max="99" name="sale_amount" value="1">
-                                </p>
+                                <h3 class="product-name"><?=$item->product_name?></h3>
+                                <p class="product-description"><?=$item->product_description?></p>
+                                <p class="product-description">Precio: $<?=$item->product_unitCost?></p>
+                                <div class="product-description" id="amount">
+                                    <label for="select-amount">Cantidad: </label>
+                                    <button type="button" id="" class="button-product button-less">-</button>
+                                    <input class="select-amount text-center" type="number" min="1" max="99" name="sale_amount" value="1">
+                                    <button type="button" id="" class="button-product button-more">+</button>
+                                </div>
                             </div>
                             <button class="button-product" type="submit" name="button_product">
                                 <i class="fi fi-sr-shopping-cart-add icon-add-product"></i>
                             </button>
                         </form>
                     </div>
+                    <?php endforeach;?>
+                    <!-- Fin de la consulta de productos -->
                 </div>
 
             </div>
@@ -137,5 +156,6 @@
     </main>
     <?php include_once './php_include/footer_content.hphp'?>
     <?php include_once './php_include/scripts_content.hphp'?>
+    <script src="js/inc_dec_buttons.js"></script>
 </body>
 </html>
